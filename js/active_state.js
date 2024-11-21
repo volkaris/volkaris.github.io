7dc9bec322af07bca0248b4e1e19974f24b8bbf1
Ilya <zaxsad109@gmail.com>
2024-11-21 17:10:17 +0300

    /

---
+++ js/active_state.js
@@ -0,0 +1,14 @@
+let locationArr = document.location.pathname.split("/")
+
+let loc = locationArr.at(-1)
+
+if (loc === "index.html") {
+    let homeBtn = document.getElementById("home")
+    homeBtn.classList.add("active")
+} else if (loc === "search.html") {
+    let searchBtn = document.getElementById("search")
+    searchBtn.classList.add("active")
+} else if (loc === "addToCart.html") {
+    let addToCartBtn = document.getElementById("shopping-bag");
+    addToCartBtn.classList.add("active")
+}
